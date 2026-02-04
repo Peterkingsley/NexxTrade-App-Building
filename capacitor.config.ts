ca64152b2ae4ee92e8617ec524e35dba836a4bca
Peterkingsley <84482319+Peterkingsley@users.noreply.github.com>
2026-02-04 18:38:33 +0100

    Modify server configuration in capacitor.config.ts
    Updated server URL and navigation settings in capacitor.config.ts.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,10 +3,16 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.nexxtrade.io',
   appName: 'NexxTrade',
-  webDir: 'dist',
+  webDir: 'dist', 
   server: {
-    url: 'https://app.nexxtrade.io', // PUT YOUR LIVE SITE HERE
-    allowNavigation: ['your-website-link.com']
+    // 1. This is the page the app opens first
+    url: 'https://www.app.nexxtrade.io', 
+    
+    // 2. This PREVENTS the app from opening the browser
+    allowNavigation: [
+      'app.nexxtrade.io',
+      '*.app.nexxtrade.io' // This covers sub-pages too
+    ]
   }
 };
 
